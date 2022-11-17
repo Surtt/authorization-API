aba76316b323bd603d12ccf25b95982f4c6b51ef
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-17 13:43:27 +0200

    add inversify and decorators

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import {App} from "./app";
+import {LoggerService} from "./logger/logger.service";
+import {UserController} from "./users/users.controller";
+import {ExceptionFilter} from "./errors/exception.filter";
+import {Container} from "inversify";
+import {ILogger} from "./logger/logger.interface";
+import {TYPES} from "./types";
+import {IExceptionFilter} from "./errors/exception.filter.interface";
+
+
+const appContainer = new Container();
+appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+appContainer.bind<UserController>(TYPES.UserController).to(UserController);
+appContainer.bind<App>(TYPES.Application).to(App);
+const app = appContainer.get<App>(TYPES.Application)
+app.init();
+
+export { app, appContainer };

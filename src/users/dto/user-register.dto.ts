065d6a0cd733932c41c53fd8eaa46529695da375
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-19 22:38:37 +0200

    add middleware and validator

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Email is incorrect' })
 	email: string;
+
+	@IsString({ message: 'Password is not specified' })
 	password: string;
+
+	@IsString({ message: 'Name is not specified' })
 	name: string;
 }

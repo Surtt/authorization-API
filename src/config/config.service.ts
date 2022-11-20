3b1bc263ef8b0b333b563969ff3fded6487d8beb
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-11-20 22:27:33 +0200

    add prisma and config service

---
+++ src/config/config.service.ts
@@ -0,0 +1,22 @@
+import { IConfigService } from './config.service.interface';
+import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+		const result: DotenvConfigOutput = config();
+		if (result.error) {
+			this.logger.error('[ConfigService] The .env file could not be read or is missing');
+		} else {
+			this.logger.log('[ConfigService] Configuration loaded');
+			this.config = result.parsed as DotenvParseOutput;
+		}
+	}
+	get(key: string): string {
+		return this.config[key];
+	}
+}
